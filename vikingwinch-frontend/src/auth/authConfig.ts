239647d7cefef365c0a7b62dd7c0fab9a01abe09
geronimo <greekgodjones@gmail.com>
2026-08-24 16:24:26 +0100

    Moved credentials to .env

--- vikingwinch-frontend/src/auth/authConfig.ts
+++ vikingwinch-frontend/src/auth/authConfig.ts
@@ -1,9 +1,9 @@
-import { PublicClientApplication, Configuration } from "@azure/msal-browser";
+import { PublicClientApplication, type Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: "YOUR_CLIENT_ID",
-        authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",
+        clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
         redirectUri: "http://localhost:5173", // Vite's default port
     },
     cache: {
